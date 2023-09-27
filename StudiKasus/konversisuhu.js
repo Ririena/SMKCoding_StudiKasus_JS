9084602a4510228bd783de1fc1966fdd7e3e8d30
Ririena <142820574+Ririena@users.noreply.github.com>
2023-09-27 23:01:02 +0700

    Add files via upload

---
+++ StudiKasus/konversisuhu.js
@@ -0,0 +1,16 @@
+function run(c) {
+  const fahreinheit = c * 9 / 5 + 32; // Rumus Konversi
+    return parseFloat(fahreinheit.toFixed(3));
+}
+
+// Coba Output
+// console.log(run(0));
+// console.log(run(5.11));
+// console.log(run(12.3));
+// console.log(run(25.5));
+// console.log(run(33.12));
+// console.log(run(50.6));
+// console.log(run(75.54));
+// console.log(run(95.2));
+// console.log(run(112.2));
+// console.log(run(125.42));
