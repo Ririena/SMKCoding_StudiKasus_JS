f39a343bce9acc94a9ccde7fa95cc56acb865ea8
Ririena <142820574+Ririena@users.noreply.github.com>
2023-09-29 22:27:38 +0700

    Update hadiahbagidua.js

--- Asasmen/hadiahbagidua.js
+++ Asasmen/hadiahbagidua.js
@@ -1,16 +1,19 @@
 function run(level, totalLevel) {
-var totalHadiah = 0;
-var pemainGagal = 20000;
-var pemainMenang = 100000;
+    var totalHadiah = 0;
+    var PemainGagal = 20000;
+    var pemainMenang = 100000;
 
-hasil = 0;
+    hasil = 0;
+    for (i = 0; i < level; i++) (
+        totalHadiah += pemainMenang)
+    if (level < totalLevel) {
+        loser = totalLevel - level
+        hasil = loser * PemainGagal
+        totalhasil = totalHadiah - hasil;
+        if (totalhasil < 0) {
+            totalhasil = 0;
+        }
 
-  for(i = 0; i < Level; i++) {
-totalHadiah += pemainMenang;
-  } if (Level < totalLevel) {
-loser = totalLevel - Level
-hasil = loser * pemainGagal;
-  }
-return 0;
-
+    }
+    return totalhasil;
 }
