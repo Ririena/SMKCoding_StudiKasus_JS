e7b2833597a6beab1e31f0fe34f8087ea1714444
Ririena <142820574+Ririena@users.noreply.github.com>
2023-09-29 20:45:42 +0700

    hadiahbagidua.js

--- Asasmen/hadiahbagidua.js
+++ Asasmen/hadiahbagidua.js
@@ -1,1 +1,16 @@
+function run(level, totalLevel) {
+var totalHadiah = 0;
+var pemainGagal = 20000;
+var pemainMenang = 100000;
 
+hasil = 0;
+
+  for(i = 0; i < Level; i++) {
+totalHadiah += pemainMenang;
+  } if (Level < totalLevel) {
+loser = totalLevel - Level
+hasil = loser * pemainGagal;
+  }
+return 0;
+
+}
